4702aea180365c86015785dab9154e361d351ecb
Cemyld <cemyld@gmail.com>
2014-06-26 12:49:35 +0300

    Added flightrule and changeofspeed classes, declared all bool checks in route class

--- RouteParser/Route.cs
+++ RouteParser/Route.cs
@@ -8,11 +8,54 @@ namespace RouteParser
 {
     class Route
     {
+        public static bool isPoint(string candidate)
+        {
+            return isCoordinatePoint(candidate)|isNavaidPoint(candidate)|isNamedPoint(candidate);
+        }
+        public static bool isNamedPoint(string candidate)
+        {
+            //Check name length
+            return Regex.IsMatch(candidate, @"\w{2,5}");
+        }
+        public static bool isCoordinatePoint(string candidate)
+        {
+            //Latitude and Longitude OR Degrees and Minutes
+            return Regex.IsMatch(candidate, @"\d{2}[N,S]\d{3}[E,W]") | Regex.IsMatch(candidate, @"\d{4}[N,S]\d{5}[E,W]");
+        }
+        public static bool isNavaidPoint(string candidate)
+        {
+            return Regex.IsMatch(candidate, @"\w{2,3}\d{3}\d{3}");
+        }
+        public static bool isSpeedLevel(string candidate)
+        {
+            return Regex.IsMatch(candidate, @"(?<speed>[K,N]\d{4}|M\d{3})(?<clevel>([A,F]\d{3})|[S,M]\d{4})");
+        }
+        public static bool isAirway(string candidate)
+        {
+            return Regex.IsMatch(candidate, @"\w{2,7}");
+        }
+        public static bool isChangeOfSpeedLevelPoint(string candidate)
+            {
+                try{
+                string [] candidateparts = candidate.Split(new char['/']);
+                string point = candidateparts[0];
+                string speedlevel = candidateparts[1];
+                return isPoint(point) & isSpeedLevel(speedlevel);
+                }
+                catch (Exception e){
+                    return false;
+                }
+            }
+        public static bool isChangeOfFlightRule(string candidate)
+        {
+            return Regex.IsMatch(candidate, @"IFR|VFR");
+        }
         interface IRouteElement
         {
             string getRepresentation();
         }
-        interface IPoint: IRouteElement{
+        interface IPoint : IRouteElement
+        {
             string latitude { get; }
             string longitude { get; }
         }
@@ -38,20 +81,17 @@ namespace RouteParser
             }
 
 
-            public static bool includes(string candidate)
-            {
-                return Regex.IsMatch(candidate, @"\w{2,5}");
-            }
+
             public string getRepresentation()
             {
-                
-                    return this._name;
+
+                return this._name;
             }
             //NOT YET IMPLEMENTED
             public string latitude { get { return string.Empty; } }
             //NOT YET IMPLEMENTED
             public string longitude { get { return string.Empty; } }
-        }  
+        }
         class CoordinatePoint : IPoint
         {
             private string _latitude;
@@ -87,11 +127,6 @@ namespace RouteParser
             public string getRepresentation()
             {
                 return string.Format("{0}{1}", this._latitude, this._longitude);
-            }
-            public static bool includes(string candidate)
-            {
-                //Latitude and Longitude OR Degrees and Minutes
-                return Regex.IsMatch(candidate, @"\d{2}[N,S]\d{3}[E,W]") | Regex.IsMatch(candidate, @"\d{4}[N,S]\d{5}[E,W]");
             }
         }
         class NavaidPoint : IPoint
@@ -145,12 +180,9 @@ namespace RouteParser
             public string longitude { get { return string.Empty; } }
             public string getRepresentation()
             {
-                return string.Format("{0}{1}{2}", this._navaidname, this._bearing,this._distance);
+                return string.Format("{0}{1}{2}", this._navaidname, this._bearing, this._distance);
             }
-            public static bool includes(string candidate)
-            {
-                return Regex.IsMatch(candidate, @"\w{2,3}\d{3}\d{3}");
-            }
+
         }
         class SpeedLevel : IRouteElement
         {
@@ -166,10 +198,7 @@ namespace RouteParser
             {
                 return string.Format("{0}{1}", this._cruisingspeed, this._flightlevel);
             }
-            public static bool includes(string candidate)
-            {
-                return Regex.IsMatch(candidate, @"(?<speed>[K,N]\d{4}|M\d{3})(?<clevel>([A,F]\d{3})|[S,M]\d{4})");
-            }
+
         }
         class Airway : IRouteElement
         {
@@ -182,10 +211,7 @@ namespace RouteParser
             {
                 return this._designator;
             }
-            public static bool includes(string candidate)
-            {
-                return Regex.IsMatch(candidate, @"\w{2,7}");
-            }
+
         }
         class ChangeOfSpeedLevelPoint : IRouteElement
         {
@@ -201,14 +227,27 @@ namespace RouteParser
             {
                 return string.Format("{0}{1}", this._point, this._speedlevel);
             }
+
         }
-        private List<IRouteElement> _path;        
+        class ChangeOfFlightRule : IRouteElement
+        {
+            private string _flightrule;
+            public ChangeOfFlightRule(string flightrule)
+            {
+                this._flightrule = flightrule;
+            }
+            public string getRepresentation()
+            {
+                return this._flightrule;
+            }
+        }
+        private List<IRouteElement> _path;
         public Route()
         {
             this._path = new List<IRouteElement>();
         }
 
-        
+
         public static Route parseString(string routestring)
         {
             Route result = new Route();
